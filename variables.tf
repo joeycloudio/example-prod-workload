2a07335023ae5f7e2a00a808228d8a97a96f3861
joeycloudio <joeyacostax@gmail.com>
2025-04-12 01:11:00 -0700

    updated variables file

--- variables.tf
+++ variables.tf
@@ -5,13 +5,3 @@ variable "aws_region" {
   description = "The AWS region you wish to deploy your resources to."
   default     = "us-east-1"
 }
-
-variable "codestar_connection_arn" {
-  type        = string
-  description = "The ARN of the CodeStar Connection used for GitHub source integration"
-}
-
-variable "dockerhub_secret_arn" {
-  type        = string
-  description = "ARN of DockerHub secret stored in AWS Secrets Manager"
-}
