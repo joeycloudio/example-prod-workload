6cfc9bb702c3825784bb6f92e1a8d501e30f9dc5
joeycloudio <joeyacostax@gmail.com>
2025-04-14 00:04:56 -0700

    Added code connection ARN to variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,9 @@ variable "aws_region" {
   description = "The AWS region you wish to deploy your resources to."
   default     = "us-east-1"
 }
+
+variable "codestar_connection_arn" {
+  description = "CodeStar Connection ARN for GitHub integration"
+  type        = string
+  default     = "arn:aws:codeconnections:us-east-1:273354636904:connection/5c6ddf43-8daf-4a6b-828a-9376e12b1408"
+}
